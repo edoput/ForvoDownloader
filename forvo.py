ee452b80b9131da3c2f01ef8005deaa6169d1196
Edoardo Putti <edoardo.putti@gmail.com>
2013-09-15 13:09:54 +0200

    Update forvo.py

--- forvo.py
+++ forvo.py
@@ -1,35 +1,36 @@
 import requests
 
-
 def Main(myfile, lang, apikey, limit):
       with open(myfile) as words:
-            n=0
-            while n < 250:
-                  for i in words:
+            for i in words:
+                  print 'Searching {0}'.format(i)
+            
+                  #here some ceaning in word format and something else
+                  s = CorrectFormat(i)
                   
-                        #here some ceaning in wod format and something else
-                        s = CorrectFormat(i)
+                  #let's make the request
+                  r = ForvoHttpRequest('word-pronunciations',s,lang,apikey)
+                  
+                  #now we download the mp3 files from forvo server if r is "not-empty"
+                  if r:
+                        print 'Downloading {0}'.format(i)
+                        for i in range(0,limit):
+                              mp3 = requests.get(r[i])
+                              file_path = i.replace('\n','')+'.{0}'+'.mp3'
+                              file_path.format(i)
+                              
+                              with open(file_path,"wb") as out:
+                                    #we open a new mp3 file and we name it after the word we're downloading. The file it's opened in
+                                    #write-binary mode
+                                    out.write(mp3.content)
+                                    
+                  else:
+                        with open('word_not_found.txt','a') as out:
+                              out.write(i + '\n')                        
                         
-                        #let's make the request
-                        r = ForvoHttpRequest('word-pronunciations',s,lang,apikey)
-                        
-                        
-                        #now we download the mp3 files from forvo server if r is "not-empty"
-                        if r:
-                              for t in range(0,limit):
-                                    mp3 = requests.get(r[t])
-                                    file_path = .replace('\n','')+'.{}'.format(t)+'.mp3'
-                                    with open(file_path,"wb") as out:
-                                          #we open a new mp3 file and we name it after the word we're downloading. The file it's opened in
-                                          #write-binary mode
-                                          out.write(mp3.content)
-                        else:
-                              with open('word_not_found.txt','a') as out:
-                                    out.write(s)
-                        
-                        n = n+1
 def CorrectFormat(s):
       #here we prepare the string used, removing whitespace and escape character
+      
       s = s.lower()                 #everithing to lower
       s = s.lstrip()                #remove head whitespace if any
       s = s.rstrip()                #remove tail whitespace if any
@@ -40,27 +41,27 @@ def CorrectFormat(s):
         
 def ForvoHttpRequest(act, word, lang, apikey):
       #This is the url we need to use to send our request, it works when you use the 'word-pronunciations' action
-      url = 'http://apifree.forvo.com/action/{0}/format/json/word/{1}/language/{2}/order/rate-desc/limit/{3}/key/{4}'.format(act, word, lang, limit, apikey)
-
-      r = requests.get(url)
-      #r is now  HTTP-request python object and we can use it's method, including reading server's json response
-      data = r.json()
+      url = 'http://apifree.forvo.com/action/{0}/format/json/word/{1}/language/{2}/key/{3}'.format(act, word, lang, apikey)
       
-      paths = []
+      r = requests.get(url)
+      #r is now HTTP-request python object and we can use it's method, including reading server's json response
+      data = r.json()     
       
       if data:
+            #we retrieved a non empty JSON.
             #the JSON is structured like this:
             #a dictionary with 2 items, their keys are:
             #-u'attributes' (linked to info about the request we made)
             #-u'items' (linked to a list of dictionaries)
-            #in the list there is a dictionary for every single pronunciation, we will search for the "mp3path" key
-            
-            if data[u'items']:
-                  for i in data[u'items']:
-                        paths.append(data[u'items'][i][u'pathmp3'])
-            else:
-                  print "{0} isn't pronounced yet on Forvo.".format(word, lang)
+            #in the list there is a dictionary for every pronunciation, we will search for the "mp3path" key
+            for i in data[u'items']:
+                  #going throgt the list an appending mp3 link to paths
+                  paths.append(i[u'pathmp3'])
+                  return paths
+                  
       else:
-            print "There isn't any JSON"
-            
-      return paths
+            #We retrieved an empty JSON beacuse there isn't any pronunciation
+            return None
+
+Main('parole_da_trovare.txt','da',APIKEY,1)
+      
